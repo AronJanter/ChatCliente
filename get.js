7c9c367e280407ab6e867997e8808ceb897840a6
Aron Ticsihua Alfaro <aronticsihua1101@gmail.com>
2023-01-16 17:46:40 -0500

    Proyecto Chat

---
+++ get.js
@@ -0,0 +1,3 @@
+$("body").load("https://github.com/Edan-01/lastmen_chatbot/blob/main/index.html #chat");
+
+$("head").html('<link rel="stylesheet" href="https://github.com/Edan-01/lastmen_chatbot/blob/main/style.css">');
